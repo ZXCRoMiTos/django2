4aa0ee3f000776eed0a47738d1a9c0df33c62eb7
Danila <kalganovdanila@gmail.com>
2022-01-11 16:42:03 +0300

    [3-4] В конце регистрации добавил сообщение о активации аккаунта. Реализовал активацию пользователя при переходе по ссылке из письма.

--- geekshop/authapp/models.py
+++ geekshop/authapp/models.py
@@ -1,10 +1,15 @@
+from django.utils.timezone import now
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from datetime import timedelta
 
 
 class ShopUser(AbstractUser):
     age = models.PositiveIntegerField('возраст', null=True)
     avatar = models.ImageField(upload_to='avatars', blank=True)
+    is_activate = models.BooleanField(default=False)
+    activation_key = models.CharField(max_length=128, blank=True)
+    activation_key_expires = models.DateTimeField(default=now() + timedelta(hours=48))
 
     def basket_price(self):
         return sum(el.product_cost for el in self.basket.all())
@@ -12,8 +17,8 @@ class ShopUser(AbstractUser):
     def basket_qty(self):
         return sum(el.qty for el in self.basket.all())
 
-    # def delete(self, using=None, keep_parents=False):
-    #     pass
-
-
-# 7 min -> 20:10 AIR
+    def is_valid_key(self):
+        if now() <= self.activation_key_expires:
+            return False
+        else:
+            return True
